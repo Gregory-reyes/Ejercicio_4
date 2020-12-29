7dcb56c935b4235027462aeebdc020ec980886fd
Gregory Reyes <greyes3@udi.edu.co>
2020-12-29 14:40:12 -0500

    Uploading Files

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ var config = require('./config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var leaders = require('./routes/leaders');
 var promotions = require('./routes/promotions')
+var leaders = require('./routes/leaders');
+var uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -36,7 +38,6 @@ app.all('*', (req, res, next) => {
     res.redirect(307,'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
 })
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -62,6 +63,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promotions);
 app.use('/leaders',leaders);
+app.use('/imageUpload',uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
